538e420f868e17bdae876942dc106a72fb367959
Alyaamerr <alya.amerrudin@gmail.com>
2021-12-19 01:07:01 +0000

    complete basic

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name = "";
+    let gender = "";
+  
+    async function getGender() {
+      fetch("https://api.genderize.io/?name=" + name)
+        .then((response) => response.json())
+        .then((data) => {
+          gender = data.gender;
+        });
+    }
+  </script>
+  
+  <h1>Guess gender</h1>
+  
+  <form on:submit|preventDefault={getGender}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+  </form>
+  
+  <p>Gender: {gender}</p>
